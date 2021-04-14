2e1c909eeab3a4bb384aa36852311912cfb29da9
elrix <xcom541@gmail.com>
2021-04-14 19:38:34 +0300

    [Renderer] generic page get config

--- plugins/newsletter/src/frontend/styles.ts
+++ plugins/newsletter/src/frontend/styles.ts
@@ -9,6 +9,8 @@ export const useStyles = makeStyles((theme: Theme) =>
             backgroundColor: '#fff',
             padding: '5px 10px',
             borderRadius: '8px 0 0 8px',
+            border: '1px solid #ccc',
+            borderRight: '0',
         },
         subscribeBtn: {
             borderRadius: '0 8px 8px 0'
--- system/renderer/src/common/createGetStaticProps.ts
+++ system/renderer/src/common/createGetStaticProps.ts
@@ -8,13 +8,16 @@ export const createGetStaticProps = (pageName: BasePageNames | string,
     return async function (context: StaticPageContext): Promise<
         { props: TCromwellPageCoreProps; revalidate?: number }> {
 
+        const pageConfigName = (pageName === 'pages/[slug]' && context?.params?.slug) ?
+            `pages/${context.params.slug}` : pageName;
+
         const serialize = (data: any) => JSON.parse(JSON.stringify(data ?? null));
 
         const apiClient = getRestAPIClient();
         const timestamp = Date.now();
         const childStaticProps = await getThemeStaticProps(pageName, pageGetStaticProps, context);
-        const { pluginsData, pluginsSettings } = await pluginsDataFetcher(pageName, context);
-        const pageConfig: TPageConfig | null = serialize(await apiClient?.getPageConfig(pageName));
+        const { pluginsData, pluginsSettings } = await pluginsDataFetcher(pageConfigName, context);
+        const pageConfig: TPageConfig | null = serialize(await apiClient?.getPageConfig(pageConfigName));
         const themeConfig: TThemeConfig | null = serialize(await apiClient?.getThemeConfig());
         const cmsSettings = serialize(await apiClient?.getCmsSettings());
         const themeCustomConfig = serialize(await apiClient?.getThemeCustomConfig());
@@ -25,9 +28,6 @@ export const createGetStaticProps = (pageName: BasePageNames | string,
         const headHtml = themeConfig?.headHtml ?? null;
         const palette = themeConfig?.palette ?? null;
 
-        // if (context && context.params && context.params.slug) {
-        //     pageRoute += '/' + context.params.slug;
-        // }
         console.log('getStaticProps for page: ' + pageName);
         console.log('time elapsed: ' + (timestamp2 - timestamp) + 'ms')
         // console.log('pluginssData', pluginsData, 'childStaticProps', childStaticProps);
