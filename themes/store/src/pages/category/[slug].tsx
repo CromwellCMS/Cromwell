60140d865abe16ce7f0d622939ce5b1ca7d038d7
eilrix <41447729+eilrix@users.noreply.github.com>
2021-06-19 23:42:53 +0300

    [Cli] Add theme cli create task

--- themes/store/src/pages/category/[slug].tsx
+++ themes/store/src/pages/category/[slug].tsx
@@ -11,116 +11,116 @@ import commonStyles from '../../styles/common.module.scss';
 import styles from '../../styles/pages/Category.module.scss';
 
 interface CategoryProps {
-    category?: TProductCategory | null;
-    products?: TPagedList<TProduct> | null;
-    attributes?: TAttribute[];
-    slug?: string;
+  category?: TProductCategory | null;
+  products?: TPagedList<TProduct> | null;
+  attributes?: TAttribute[];
+  slug?: string;
 }
 
+
 const ProductCategory: TCromwellPage<CategoryProps> = (props) => {
-    // console.log('ProductThemePage props', props);
-    const category = props.category;
-    const client = getGraphQLClient();
-    const listId = 'Category_ProductList';
-    return (
-        <Layout>
-            <div className={commonStyles.content}>
-                <div className={styles.content}>
-                    <div className={styles.sidebar}>
-                        <CContainer id="Category_ProductFilter" />
-                    </div>
-                    <div className={styles.main}>
-                        <div className={styles.sortContainer}>
-                            <CategorySort listId={listId} />
-                        </div>
-                        {category && props.attributes && (
-                            <CList<TProduct>
-                                id={listId}
-                                ListItem={(p) => {
-                                    return (
-                                        <div className={styles.productWrapper}>
-                                            <ProductCard
-                                                data={p.data}
-                                                className={styles.product}
-                                                key={p.data?.id}
-                                                attributes={props.attributes}
-                                            />
-                                        </div>
-                                    )
-                                }}
-                                usePagination
-                                useShowMoreButton
-                                useQueryPagination
-                                disableCaching
-                                pageSize={20}
-                                maxDomPages={2}
-                                scrollContainerSelector={`.${layoutStyles.Layout}`}
-                                firstBatch={props.products ? props.products : undefined}
-                                loader={async (params) => {
-                                    return client?.getProductsFromCategory(category.id, params)
-                                }}
-                                cssClasses={{
-                                    page: styles.productList
-                                }}
-                                elements={{
-                                    pagination: Pagination
-                                }}
-                            />
-                        )}
-                    </div>
-                </div>
+  // console.log('ProductThemePage props', props);
+  const category = props.category;
+  const client = getGraphQLClient();
+  const listId = 'Category_ProductList';
+  return (
+    <Layout>
+      <div className={commonStyles.content}>
+        <div className={styles.content}>
+          <div className={styles.sidebar}>
+            <CContainer id="Category_ProductFilter" />
+          </div>
+          <div className={styles.main}>
+            <div className={styles.sortContainer}>
+              <CategorySort listId={listId} />
             </div>
-        </Layout>
-    );
+            {category && props.attributes && (
+              <CList<TProduct>
+                id={listId}
+                ListItem={(p) => {
+                  return (
+                    <div className={styles.productWrapper}>
+                      <ProductCard
+                        data={p.data}
+                        className={styles.product}
+                        key={p.data?.id}
+                        attributes={props.attributes}
+                      />
+                    </div>
+                  )
+                }}
+                usePagination
+                useShowMoreButton
+                useQueryPagination
+                disableCaching
+                pageSize={20}
+                maxDomPages={2}
+                scrollContainerSelector={`.${layoutStyles.Layout}`}
+                firstBatch={props.products ? props.products : undefined}
+                loader={async (params) => {
+                  return client?.getProductsFromCategory(category.id, params)
+                }}
+                cssClasses={{
+                  page: styles.productList
+                }}
+                elements={{
+                  pagination: Pagination
+                }}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export const getStaticProps: TGetStaticProps = async (context): Promise<CategoryProps> => {
-    const slug = (context && context.params) ? context.params.slug : null;
-    // console.log('CategoryThemePage::getStaticProps: slug', slug, 'context.params', context.params)
-    const client = getGraphQLClient();
+  const slug = context?.params?.slug;
+  const client = getGraphQLClient();
 
-    let category: TProductCategory | undefined | null = null;
-    if (slug && typeof slug === 'string') {
-        try {
-            category = await client?.getProductCategoryBySlug(slug);
-        } catch (e) {
-            console.error('ProductCategory::getStaticProps 1, slug: ' + slug, e)
-        }
-    } else {
-        console.error('ProductCategory::getStaticProps: !pid')
-    }
-    let products: TPagedList<TProduct> | undefined | null = null;
-    if (category) {
-        try {
-            products = await client?.getProductsFromCategory(category.id,
-                { pageSize: 20 })
-        } catch (e) {
-            console.error('ProductCategory::getStaticProps 2, slug: ' + slug, e)
-        }
-    }
-
-    let attributes: TAttribute[] | undefined;
-
+  let category: TProductCategory | undefined | null = null;
+  if (slug && typeof slug === 'string') {
     try {
-        attributes = await client?.getAttributes();
+      category = await client?.getProductCategoryBySlug(slug);
     } catch (e) {
-        console.error('Product::getStaticProps', e)
+      console.error('ProductCategory::getStaticProps 1, slug: ' + slug, e)
     }
+  } else {
+    console.error('ProductCategory::getStaticProps: !pid')
+  }
+  let products: TPagedList<TProduct> | undefined | null = null;
+  if (category) {
+    try {
+      products = await client?.getProductsFromCategory(category.id,
+        { pageSize: 20 })
+    } catch (e) {
+      console.error('ProductCategory::getStaticProps 2, slug: ' + slug, e)
+    }
+  }
 
-    return {
-        slug: slug as string,
-        category: category ? category : null,
-        products: products ? products : null,
-        attributes
-    }
+  let attributes: TAttribute[] | undefined;
+
+  try {
+    attributes = await client?.getAttributes();
+  } catch (e) {
+    console.error('Product::getStaticProps', e)
+  }
+
+  return {
+    slug: slug as string,
+    category: category ? category : null,
+    products: products ? products : null,
+    attributes
+  }
 
 }
 
 export const getStaticPaths = () => {
-    return {
-        paths: [],
-        fallback: true
-    };
+  return {
+    paths: [],
+    fallback: true
+  };
 }
 
 export default ProductCategory;
